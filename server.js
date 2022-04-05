ea171225ac3c5e9d89a3c22a687e6a801e84ae37
Prajal Patidar <prajalpatidar01@gmail.com>
2022-04-05 22:57:37 +0530

    add body-parser to solve req.body undefine error

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 require("dotenv").config();
-
+app.use(bodyParser.json());
 app.use("/", require("./routes"));
 
 mongoose
