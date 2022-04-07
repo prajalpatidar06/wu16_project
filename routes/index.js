5914c88bfff1afd33870922676f29c9052eaa344
Prajal Patidar <prajalpatidar01@gmail.com>
2022-04-07 17:31:20 +0530

    add homepage route

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,10 @@ const router = express.Router();
 
 router.use("/users", require("./users"));
 router.use("/api", require("./urls"));
+router.use("/", (req, res) => {
+  res.send(
+    "<h1>Welcome to URL-SHORTNER</h1><p>WU-16 Project made by Prajal Patidar</p>"
+  );
+});
 
 module.exports = router;
